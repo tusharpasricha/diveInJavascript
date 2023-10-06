fe0346c3ee4d57ccfcded7f00652f3739d32b1e7
Tushar Pasricha <72183174+tusharpasricha@users.noreply.github.com>
2023-10-06 05:38:55 +0000

    STRINGS

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log('hy');
+// console.log('hy');
+
+// const count = 10;
+
+// const name = "tushar"
+
+// console.log(name+ "hello" + count);
+// console.log(`${count} helle ${name}`)
+
+const name = new String('tushar');
+// console.log(name);
+// console.log(name.__proto__);
+// console.log(name.length);
+
+console.log(name.charAt(3));
+console.log(name.indexOf('h'))
